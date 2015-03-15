ae4de5698f1f5052c8538727041d0173c4ef4f81
Phillip Tischler <pmt43@cornell.edu>
2015-03-14 21:08:51 -0400

    Refactored setup files for aptitude packages to be simpler and easier to use. Added various setting components to enable deployable system. Added automatic database backup and creation. Added automatic Apache deployment.

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,6 +4,13 @@
 # Quit immediately on any error
 set -e
 
+# Create soft link from repo to standardize scripts
+if [ -h /auvsi_suas_competition ]
+then
+    sudo rm /auvsi_suas_competition;
+fi
+sudo ln -s ${PWD}/.. /auvsi_suas_competition
+
 # Update the package list
 sudo apt-get -y update
 
@@ -17,6 +24,27 @@ sudo apt-get -y install puppet
 sudo mkdir -p /etc/puppet/modules/
 sudo puppet module install -f puppetlabs/stdlib
 sudo puppet module install -f puppetlabs-apt
+sudo puppet module install -f puppetlabs-apache
+sudo puppet module install -f puppetlabs-concat
+sudo puppet module install -f puppetlabs-mysql
+sudo puppet module install -f puppetlabs-ntp
 
-# Launch the Puppet process
+# Launch the Puppet process. Installs dependencies and configures Apache.
 sudo puppet apply --modulepath=${PWD}/puppet_files:/etc/puppet/modules/:/usr/share/puppet/modules/ puppet_files/auvsi_suas.pp
+
+# Create the database with a test admin
+# (username: testadmin, password: testpass)
+(
+cd ../src/auvsi_suas_server;
+if [ ! -d data ]
+then
+    mkdir data;
+fi
+if [ -f data/db.sqlite3 ]
+then 
+    sudo mv data/db.sqlite3 "data/db.sqlite3.`date`";
+fi
+sudo python manage.py syncdb --noinput;
+sudo chown www-data:www-data data;
+sudo chown www-data:www-data data/db.sqlite3;
+)
